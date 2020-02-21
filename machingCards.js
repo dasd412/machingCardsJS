04370d03decb66a8c32b10842be7ee07396923b4
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-02-21 16:12:12 +0900

    added initialShowing

--- machingCards.js
+++ machingCards.js
@@ -1,5 +1,15 @@
 const row=4,col=3;
 
+const candidateColors=['red','red','pink','pink','green','green','white','white','orange','orange','yellow','yellow'];
+let colors=[];
+
+function randomColorSuffle(){
+
+    for(let i=0;candidateColors.length>0;i++){
+        colors=colors.concat(candidateColors.splice(Math.floor(Math.random()*candidateColors.length),1));
+    }
+}
+
 function settingCard(col,row){
     for(let i=0;i<col*row;i++){
     const card=document.createElement('div');
@@ -11,6 +21,7 @@ function settingCard(col,row){
     cardFront.className='card-front';
     const cardBack=document.createElement('div');
     cardBack.className='card-back';
+    cardBack.style.backgroundColor=colors[i];
 
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
@@ -27,6 +38,30 @@ function settingCard(col,row){
     }//for
 }
 
+function initialShowing(){
+    document.querySelectorAll('.card').forEach(function(card,index){
 
+        setTimeout(function(){
+
+            card.classList.add('flipped');
+
+        },1000+100*index);
+    });
+
+    setTimeout(function(){
+        document.querySelectorAll('.card').forEach(function(card,index){
+
+            card.classList.remove('flipped');
+        });
+    },5000);
+}
+
+function init(){
+    randomColorSuffle();
     settingCard(col,row);
+    initialShowing();
+}
+
+init();
+   
 
