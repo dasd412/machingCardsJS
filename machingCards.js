ec6cbbf412e5d74c0a8cbb2c34a81ff870ed9929
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-02-21 15:41:12 +0900

    solve closure problem

---
+++ machingCards.js
@@ -0,0 +1,34 @@
+const row=4,col=3;
+
+function settingCard(col,row){
+    for(let i=0;i<col*row;i++){
+    const card=document.createElement('div');
+    card.className='card';
+    const cardInner=docuemnt.createElement('div');
+    cardInner.className='card-inner';
+
+    const cardFront=document.createElement('div');
+    cardFront.className='card-front';
+    const cardBack=document.createElement('div');
+    cardBack.className='card-back';
+
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+    card.appendChild(cardInner);
+
+    (function(c){
+    card.addEventListener('click',function(){
+      c.classList.toggle('flipped');
+    });
+    })(card);//for solving closure problem
+    
+    document.body.appendChild(card);
+
+    }
+}
+
+function init(){
+    settingCard(col,row);
+}
+
+init();
